b9fb3254ac00f326165e7e212aa8c769040d6925
Nasibov Rafael <nasibovrafael1@gmail.com>
2024-09-05 13:50:32 +0300

    user migration fix

--- DAL/Entities/User.cs
+++ DAL/Entities/User.cs
@@ -3,11 +3,11 @@
 namespace DAL.Entities;
 
 public class User
-{    
+{
     public Guid Id { get; set; }
     public required string Username { get; set; }
     public required string Email { get; set; }
-    public required string PasswordHash { get; set; }
+    public required byte[] PasswordHash { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 }
--- DAL/Migrations/20240905104924_pwdHashDeleted.Designer.cs
+++ DAL/Migrations/20240905104924_pwdHashDeleted.Designer.cs
@@ -4,6 +4,7 @@ using DAL.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240905104924_pwdHashDeleted")]
+    partial class pwdHashDeleted
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -80,10 +83,6 @@ namespace DAL.Migrations
                     b.Property<string>("Email")
                         .IsRequired()
                         .HasColumnType("nvarchar(450)");
-
-                    b.Property<byte[]>("PasswordHash")
-                        .IsRequired()
-                        .HasColumnType("varbinary(max)");
 
                     b.Property<DateTime>("UpdatedAt")
                         .ValueGeneratedOnAddOrUpdate()
