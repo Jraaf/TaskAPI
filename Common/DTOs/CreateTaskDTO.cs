4f8329b80cd1b74911daf623ddb0d0513c792310
Nasibov Rafael <nasibovrafael1@gmail.com>
2024-09-07 19:58:29 +0300

    validation

--- Common/DTOs/CreateTaskDTO.cs
+++ Common/DTOs/CreateTaskDTO.cs
@@ -1,12 +1,14 @@
 ﻿using DAL.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace Common.DTOs;
 
 public class CreateTaskDTO
 {
+    [MinLength(3)]
     public required string Title { get; set; }
     public string? Description { get; set; }
     public Guid? UserId { get; set; }
-    public Status Status { get; set; }
-    public Priority Priority { get; set; }
+    public required Status Status { get; set; }
+    public required Priority Priority { get; set; }
 }
--- Common/DTOs/CreateUserDTO.cs
+++ Common/DTOs/CreateUserDTO.cs
@@ -1,10 +1,14 @@
 ﻿using DAL.Entities;
+using System.ComponentModel.DataAnnotations;
 
 namespace Common.DTOs;
 
 public class CreateUserDTO
 {
+    [MinLength(3)]
     public required string Username { get; set; }
+    [MinLength(3)]
     public required string Email { get; set; }
+    [MinLength(3)]
     public required string Password { get; set; }
 }
